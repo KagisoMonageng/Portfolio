ca8953ccfe213b185f40bd4dd1e642e1e40cc00e
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-06-18 18:04:13 +0200

    gsap added

--- template/src/app/app.component.ts
+++ template/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'template';
+  sec_sec = document.getElementById("2nd")
+  ngOnInit(): void {
+    gsap.to('.sec-sec', {
+      scrollTrigger: {
+        trigger: '.sec-sec',
+        pin: true,
+        start: 'top top',
+        end: '+=150',
+        pinSpacing: false
+      }
+    })
+    ScrollTrigger.create({
+      trigger: this.sec_sec,
+      start: "top top",
+      end: "bottom bottom",
+      animation: gsap.fromTo("#first-line", {
+        opacity: 0,
+        y: 100
+      }, {
+        opacity: 1,
+        y: 0,
+        duration: 1
+      }),
+      scrub: true,
+    });
+
+    ScrollTrigger.create({
+      trigger: this.sec_sec,
+      start: "top top",
+      end: "bottom bottom",
+      animation: gsap.fromTo("#second-line", {
+        opacity: 0
+      }, {
+        opacity: 1,        
+      }),
+      scrub: true,
+    });
+
+
+  }
 }
