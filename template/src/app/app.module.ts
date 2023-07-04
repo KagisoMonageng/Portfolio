0c2b820eb75439c454a9a34182d7cd5c970fc6e0
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-07-04 05:56:33 +0200

    griend added

--- template/src/app/app.module.ts
+++ template/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HorScrollComponent } from './hor-scroll/hor-scroll.component';
+import { RecentWorkComponent } from './recent-work/recent-work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HorScrollComponent
+    HorScrollComponent,
+    RecentWorkComponent
   ],
   imports: [
     BrowserModule,
